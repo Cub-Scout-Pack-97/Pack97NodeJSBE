a3ef84b56cb1bd616a7791c5907700dd3cf2d894
John Kelley <john.r.kelley@lowes.com>
2018-08-16 09:20:28 -0400

    Start creation of NodeJS API app

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FMAINTAINER John Kelley johnkelley4477@gmail.com
-
-## Use Alpine as the OS because of it's size
 FROM alpine
+## Use Alpine as the OS because of it's size
+
+MAINTAINER John Kelley johnkelley4477@gmail.com
 
 ## Update package definitions
 RUN apk update
@@ -22,10 +22,10 @@ RUN apk add --update nodejs nodejs-npm
 RUN apk add git
 
 ## Move to dev folder
-RUN git clone https://github.com/johnkelley4477/Pack97NodeJSBE.git:develop
+RUN git clone https://github.com/johnkelley4477/Pack97NodeJSBE.git
 
 ## Delete GIT
-RUN apk del git
+## RUN apk del git
 
 ## Create Working Dir
 WORKDIR /Pack97NodeJSBE
---
+++ index.js
@@ -0,0 +1,33 @@
+/**
+* This is based off a tutorial from: https://medium.freecodecamp.org/how-to-setup-a-powerful-api-with-nodejs-graphql-mongodb-hapi-and-swagger-e251ac189649
+*
+*
+**/
+
+const hapi = require('hapi'); 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://i:i@localhost:27017/pack97');
+mongoose.connection.once('open',() => {
+	console.log("connected to db");
+});
+
+const server = hapi.server({
+	port:4477,
+	host: '0.0.0.0'
+});
+
+const init = async () => {
+	server.route({
+		method:'GET',
+		path:'/',
+		handler: function(request,reply){
+			return '<hi>APIs ROCK!!!!!!!!!!!!!!!!!!!!!!</h1>';
+		}
+	});
+
+	await server.start();
+	console.log(`Server running at: ${server.info.uri}`);
+};
+
+init();
